8f1bdcab835c093ba12cb9e2ac603a8d225647a1
navid anjum chowdhury <navid.rashik@gmail.com>
2020-10-26 01:40:03 +0600

    updated storage url

--- ihost/urls.py
+++ ihost/urls.py
@@ -13,7 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
+
 from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg2.views import get_schema_view
@@ -51,4 +54,5 @@ urlpatterns = [
     path("api/", include("account_management.urls")),
 
 
-]+swagger_url
+]+swagger_url + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
+                                                                                       document_root=settings.STATIC_ROOT)
