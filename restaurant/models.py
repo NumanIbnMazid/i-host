fd4e58369b80f38fb4c6a2d246fd133ca441558c
navid.rashik <navid.rashik@gmail.com>
2020-11-02 14:16:26 +0600

    restaurant added image and sd const amout options

--- restaurant/models.py
+++ restaurant/models.py
@@ -16,23 +16,26 @@ class Restaurant(models.Model):
     RESTAURANT_STATUS = [('ACTIVE', 'ACTIVE'), ('INACTIVE', 'INACTIVE')]
 
     name = models.CharField(max_length=250)
+    logo = models.ImageField(blank=True, null=True)
     address = models.CharField(max_length=500, null=True, blank=True)
     latitude = models.DecimalField(
         max_digits=50, decimal_places=45, null=True, blank=True)
     longitude = models.DecimalField(
         max_digits=50, decimal_places=45, null=True, blank=True)
-    service_charge_percentage = models.DecimalField(
-        max_digits=4, decimal_places=2, default=00.00)
+    service_charge_is_percentage = models.BooleanField(default=False)
+    service_charge = models.DecimalField(
+        max_digits=8, decimal_places=2, default=00.00)
     tax_percentage = models.DecimalField(
         max_digits=4, decimal_places=2, default=00.00)
     created_at = models.DateTimeField(auto_now_add=True)
     website = models.URLField(null=True, blank=True)
-    status = models.CharField(choices=RESTAURANT_STATUS,max_length=25, default="ACTIVE")
+    status = models.CharField(
+        choices=RESTAURANT_STATUS, max_length=25, default="ACTIVE")
 
 
 class RestaurantContactPerson(models.Model):
     name = models.CharField(max_length=150)
-    designation = models.CharField(max_length=150 ,null=True,blank=True)
+    designation = models.CharField(max_length=150, null=True, blank=True)
     phone = models.CharField(max_length=25, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
     restaurant = models.ForeignKey(
