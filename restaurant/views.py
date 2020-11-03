12914b3b027be4e67fe3b86736c6cc7fe8ee3500
navid.rashik <navid.rashik@gmail.com>
2020-11-03 19:31:41 +0600

    customviewset working

--- restaurant/views.py
+++ restaurant/views.py
@@ -10,6 +10,7 @@ from utils.response_wrapper import ResponseWrapper
 from rest_framework import permissions, status, viewsets
 from .serializers import FoodCategorySerializer, RestaurantSerializer, RestaurantContactPerson, RestaurantUpdateSerialier
 from django.db.models import Q
+from utils.custom_viewset import CustomViewSet
 
 
 class RestaurantViewSet(viewsets.ModelViewSet):
@@ -88,41 +89,48 @@ class RestaurantViewSet(viewsets.ModelViewSet):
         return ResponseWrapper(data=serializer.data)
 
 
-class FoodCategoryViewSet(viewsets.GenericViewSet):
+# class FoodCategoryViewSet(viewsets.GenericViewSet):
+#     serializer_class = FoodCategorySerializer
+#     permission_classes = [permissions.IsAdminUser]
+#     queryset = FoodCategory.objects.all()
+#     lookup_field = 'pk'
+
+#     def list(self, request):
+#         qs = self.get_queryset()
+#         serializer = self.serializer_class(instance=qs, many=True)
+#         # serializer.is_valid()
+#         return ResponseWrapper(data=serializer.data, msg='success')
+
+#     def create(self, request):
+#         serializer = self.serializer_class(data=request.data)
+#         if serializer.is_valid():
+#             qs = serializer.save()
+#             serializer = self.serializer_class(instance=qs)
+#             return ResponseWrapper(data=serializer.data, msg='created')
+#         else:
+#             return ResponseWrapper(error_msg=serializer.errors, error_code=400)
+
+#     def update(self, request, **kwargs):
+#         serializer = self.serializer_class(data=request.data)
+#         if serializer.is_valid():
+#             qs = serializer.update(instance=self.get_object(
+#             ), validated_data=serializer.validated_data)
+#             serializer = self.serializer_class(instance=qs)
+#             return ResponseWrapper(data=serializer.data)
+#         else:
+#             return ResponseWrapper(error_msg=serializer.errors, error_code=400)
+
+#     def destroy(self, request, **kwargs):
+#         qs = self.queryset.filter(**kwargs).first()
+#         if qs:
+#             qs.delete()
+#             return ResponseWrapper(status=200, msg='deleted')
+#         else:
+#             return ResponseWrapper(error_msg="failed to delete", error_code=400)
+
+
+class FoodCategoryViewSet(CustomViewSet):
     serializer_class = FoodCategorySerializer
     permission_classes = [permissions.IsAdminUser]
     queryset = FoodCategory.objects.all()
     lookup_field = 'pk'
-
-    def list(self, request):
-        qs = self.get_queryset()
-        serializer = self.serializer_class(instance=qs, many=True)
-        # serializer.is_valid()
-        return ResponseWrapper(data=serializer.data, msg='success')
-
-    def create(self, request):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            qs = serializer.save()
-            serializer = self.serializer_class(instance=qs)
-            return ResponseWrapper(data=serializer.data, msg='created')
-        else:
-            return ResponseWrapper(error_msg=serializer.errors, error_code=400)
-
-    def update(self, request, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            qs = serializer.update(instance=self.get_object(
-            ), validated_data=serializer.validated_data)
-            serializer = self.serializer_class(instance=qs)
-            return ResponseWrapper(data=serializer.data)
-        else:
-            return ResponseWrapper(error_msg=serializer.errors, error_code=400)
-
-    def destroy(self, request, **kwargs):
-        qs = self.queryset.filter(**kwargs).first()
-        if qs:
-            qs.delete()
-            return ResponseWrapper(status=200, msg='deleted')
-        else:
-            return ResponseWrapper(error_msg="failed to delete", error_code=400)
