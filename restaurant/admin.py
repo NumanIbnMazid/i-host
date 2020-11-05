064918cf42636f71b6e86755bfda73aeca7d4ed1
shojibhasan <shojibhasan15@gmail.com>
2020-11-05 16:17:39 +0600

    food in admin

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Restaurant, Subscription
+from .models import Restaurant, Subscription,Food
 # Register your models here.
 admin.site.register(Restaurant)
 admin.site.register(Subscription)
+admin.site.register(Food)
