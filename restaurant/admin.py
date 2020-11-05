405207767d7ef0340004ceb16ea94fd8c565f5b0
shojibhasan <shojibhasan15@gmail.com>
2020-11-05 16:22:09 +0600

    FoodCategory in admin

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Restaurant, Subscription,Food
+from .models import Restaurant, Subscription,Food,FoodCategory
 # Register your models here.
 admin.site.register(Restaurant)
 admin.site.register(Subscription)
 admin.site.register(Food)
+admin.site.register(FoodCategory)
