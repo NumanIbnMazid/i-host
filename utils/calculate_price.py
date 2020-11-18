12e89b3a4c9d87e2e32d467509345eebaeab17b1
navid.rashik <navid.rashik@gmail.com>
2020-11-18 17:51:09 +0600

    calculate payment update

--- utils/calculate_price.py
+++ utils/calculate_price.py
@@ -13,6 +13,7 @@ def calculate_price(food_order_obj):
     tax_amount = decimal.Decimal(0.0)
     grand_total_price = decimal.Decimal(0.0)
     service_charge = decimal.Decimal(0.0)
+    hundred = decimal.Decimal(100.0)
     for ordered_item in ordered_items_qs:
         if not restaurant_qs:
             restaurant_qs=ordered_item.food_option.food.restaurant
@@ -29,11 +30,11 @@ def calculate_price(food_order_obj):
 
     if restaurant_qs.service_charge_is_percentage:
         service_charge = grand_total_price * \
-            restaurant_qs.service_charge/100
+            (restaurant_qs.service_charge/hundred)
     else:
         service_charge = grand_total_price + \
             restaurant_qs.service_charge
     grand_total_price += service_charge
-    tax_amount = grand_total_price * restaurant_qs.tax_percentage/100
+    tax_amount = grand_total_price * (restaurant_qs.tax_percentage/hundred)
     grand_total_price += tax_amount
     return {"grand_total_price": grand_total_price, "tax_amount": tax_amount, "service_charge": service_charge, 'total_price': grand_total_price}
