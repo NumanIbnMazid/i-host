43daf15dfb32138f9b037d3fdfa8f7f57ae01b5c
navid.rashik <navid.rashik@gmail.com>
2020-11-23 16:55:24 +0600

    updated calculate price and food order serializer

--- utils/calculate_price.py
+++ utils/calculate_price.py
@@ -1,9 +1,13 @@
 import decimal
 
 
-def calculate_price(food_order_obj):
-    ordered_items_qs = food_order_obj.ordered_items.exclude(
-        status__in=["4_CANCELLED", "0_ORDER_INITIALIZED"])
+def calculate_price(food_order_obj, include_initial_order=False):
+    if include_initial_order:
+        ordered_items_qs = food_order_obj.ordered_items.exclude(
+            status__in=["4_CANCELLED"])
+    else:
+        ordered_items_qs = food_order_obj.ordered_items.exclude(
+            status__in=["4_CANCELLED", "0_ORDER_INITIALIZED"])
 
     restaurant_qs = None
     if food_order_obj.table:
@@ -16,7 +20,7 @@ def calculate_price(food_order_obj):
     hundred = decimal.Decimal(100.0)
     for ordered_item in ordered_items_qs:
         if not restaurant_qs:
-            restaurant_qs=ordered_item.food_option.food.restaurant
+            restaurant_qs = ordered_item.food_option.food.restaurant
         item_price = ordered_item.quantity*ordered_item.food_option.price
         extra_price = ordered_item.quantity * sum(
             list(
@@ -25,8 +29,6 @@ def calculate_price(food_order_obj):
         )
         total_price += item_price+extra_price
     grand_total_price += total_price
-    
-    
 
     if restaurant_qs.service_charge_is_percentage:
         service_charge = grand_total_price * \
@@ -37,4 +39,9 @@ def calculate_price(food_order_obj):
     grand_total_price += service_charge
     tax_amount = grand_total_price * (restaurant_qs.tax_percentage/hundred)
     grand_total_price += tax_amount
-    return {"grand_total_price": grand_total_price, "tax_amount": tax_amount, "service_charge": service_charge, 'total_price': total_price}
+    return {
+        "grand_total_price": grand_total_price,
+        "tax_amount": tax_amount,
+        'tax_percentage': restaurant_qs.tax_percentage,
+        "service_charge": service_charge,
+        'total_price': total_price}
