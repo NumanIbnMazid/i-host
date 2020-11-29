31c2094c13e90a58c962c576fc72d4aa60befe24
sakilanasrinsetu <setusakilanasrin@gmail.com>
2020-11-29 10:58:45 +0600

    fix

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -182,12 +182,14 @@ urlpatterns = [
 
     path('restaurant/<int:restaurant>/discount_list/',
          DiscountViewSet.as_view({'get': 'discount_list'}), name='discount_list'),
-    path('dashboard/', include(dashboard_urls))
+
 
     path('delete_discount/<int:discount_id>',
          DiscountViewSet.as_view({'delete': 'discount_delete'}), name='discount_delete'),
     #path('update_discount/<int:pk>',
      #    DiscountViewSet.as_view({'patch': 'update_discount'}), name='update_discount'),
+
+    path('dashboard/', include(dashboard_urls)),
 
 
 ]
