dcae2160d8c7054077e30b3b7402638971cd4216
navid.rashik <navid.rashik@gmail.com>
2020-12-17 16:37:55 +0600

    fix socket deploy issue

--- ihost/asgi.py
+++ ihost/asgi.py
@@ -25,7 +25,7 @@ from restaurant.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    #"https": get_asgi_application(),
+    "https": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
             websocket_urlpatterns
