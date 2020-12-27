61bd34f7794f667e5f648f9da38521b1cc85b9e8
sakilanasrinsetu <setusakilanasrin@gmail.com>
2020-12-27 14:31:16 +0600

    create updat

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -567,15 +567,24 @@ class RestaurantPostSerialier(serializers.ModelSerializer):
 
     def create(self, validated_data):
         logo = validated_data.pop('logo', None)
-        if logo:
-            return Restaurant.objects.create(logo=logo, **validated_data)
-        return Restaurant.objects.create(**validated_data)
+        payment_type = validated_data.pop('payment_type', [])
 
-    def update(self, validated_data):
-        logo = validated_data.pop('logo', None)
         if logo:
-            return Restaurant.objects.create(logo=logo, **validated_data)
-        return Restaurant.objects.create(**validated_data)
+            qs= Restaurant.objects.create(logo=logo, **validated_data)
+        else:
+            qs = Restaurant.objects.create(**validated_data)
+        qs.payment_type.set(payment_type)
+        qs.save()
+        return qs
+
+    def update(self,instance, validated_data):
+        logo = validated_data.pop('logo', None)
+
+
+        if logo:
+            instance.logo = logo
+            instance.save()
+        return  super(RestaurantPostSerialier,self).update(instance,validated_data)
 
 
 class RestaurantUpdateSerialier(serializers.ModelSerializer):
