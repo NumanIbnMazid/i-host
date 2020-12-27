d2e3ccf22df0d6f6d8933d23d2c5c66bb2404550
shojibhasan <shojibhasan15@gmail.com>
2020-12-27 12:11:43 +0600

    restaurant_payment_type api error fixed

--- restaurant/views.py
+++ restaurant/views.py
@@ -2130,12 +2130,13 @@ class RestaurantMessagesViewset(LoggingMixin, CustomViewSet):
 
 
 class PaymentTypeViewSet(LoggingMixin, CustomViewSet):
-    permission_classes = [permissions.IsAdminUser]
     queryset = PaymentType.objects.all()
     lookup_field = 'pk'
     serializer_class = PaymentTypeSerializer
 
+
     def restaurant_payment_type(self, request, restaurant, *args, **kwargs):
-        payment_type_qs = Restaurant.objects.filter(payment_type_id=restaurant)
-        serializer = PaymentTypeSerializer(instance=payment_type_qs,many=True)
+        restaurant = Restaurant.objects.filter(id=restaurant).last()
+        qs = restaurant.payment_type.all()
+        serializer = PaymentTypeSerializer(instance=qs, many=True)
         return ResponseWrapper(data=serializer.data)
