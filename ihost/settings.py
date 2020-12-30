79fc19122085a608f738042dbd9438568bd3ce48
navid.rashik <navid.rashik@gmail.com>
2020-12-30 13:40:38 +0600

    redis removed

--- ihost/settings.py
+++ ihost/settings.py
@@ -283,13 +283,13 @@ CACHES = {
 }
 
 CHANNEL_LAYERS = {
-    # "default": {
-    #     "BACKEND": "channels.layers.InMemoryChannelLayer"
-    # }
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+    # 'default': {
+    #     'BACKEND': 'channels_redis.core.RedisChannelLayer',
+    #     'CONFIG': {
+    #         "hosts": [('127.0.0.1', 6379)],
+    #     },
+    # },
 }
