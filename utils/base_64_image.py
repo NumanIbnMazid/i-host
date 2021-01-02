ce8014915372dc5bf1bb810c2b768c7e86a5ed8d
navid.rashik <navid.rashik@gmail.com>
2021-01-02 12:53:09 +0600

    testing base64 svg

--- utils/base_64_image.py
+++ utils/base_64_image.py
@@ -2,11 +2,5 @@ from drf_extra_fields import fields
 # .fields import Base64ImageField
 
 
-class Base64ImageField(fields.Base64ImageField):
-    ALLOWED_TYPES = (
-        "jpeg",
-        "jpg",
-        "png",
-        "gif",
-        "svg",
-    )
+class Base64ImageField(fields.Base64FileField):
+    pass
