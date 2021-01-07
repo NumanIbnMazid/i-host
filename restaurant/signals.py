cf72ac7d122167848a0d8c16d3e846681d566627
navid.rashik <navid.rashik@gmail.com>
2021-01-07 13:03:14 +0600

    signal updated

--- restaurant/signals.py
+++ restaurant/signals.py
@@ -107,7 +107,7 @@ def kitchen_items_print(sender, qs=None, *args, **kwargs):
     context = {
         'table_no': qs.values_list('food_order__table__table_no', flat=True).distinct().last(),
         'order_id': qs.values_list('food_order_id', flat=True).distinct().last(),
-        'time': str(timezone.now().date()) + '  ' + str(timezone.now().time()),
+        'time': str(timezone.now().strftime('/%d/%m/%Y %H:%M')),
         'items_data': serializer.data
     }
     html_string = render_to_string('invoice.html', context)
