ff6a3b46cba4530ee2137046cb1db512a0a6de64
navid.rashik <navid.rashik@gmail.com>
2021-01-18 11:28:49 +0600

    socket implemented for apps

--- restaurant/routing.py
+++ restaurant/routing.py
@@ -6,5 +6,9 @@ from django.core.asgi import get_asgi_application
 websocket_urlpatterns = [
     path(r'ws/dashboard/<int:restaurant_id>/',
          consumers.DashboardConsumer.as_asgi()),
+    path(r'ws/apps/customer/<int:order_id>/',
+         consumers.AppsConsumer.as_asgi()),
+    path(r'ws/apps/waiter/<int:waiter_id>/',
+         consumers.AppsConsumer.as_asgi()),
     # re_path(r"", get_asgi_application()),
 ]
