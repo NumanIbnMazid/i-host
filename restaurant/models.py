53368452ef61433f1aa7197f46467b5b3e8a50b1
navid.rashik <navid.rashik@gmail.com>
2021-01-25 14:57:30 +0600

    migrations++

--- restaurant/models.py
+++ restaurant/models.py
@@ -217,6 +217,7 @@ class FoodOrder(SoftDeleteModel):
         to='account_management.CustomerInfo', on_delete=models.SET_NULL, null=True, blank=True, related_name='food_orders')
     restaurant = models.ForeignKey(
         to=Restaurant, on_delete=models.SET_NULL, null=True, blank=True, related_name='food_orders')
+    applied_promo_code = models.CharField(max_length=250,null=True,blank=True)
 
     def __str__(self):
         if self.order_no:
