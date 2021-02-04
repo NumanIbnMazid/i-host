4a282ef7865572142f85ddfac8f8b8ef1edd02a2
sakilanasrinsetu <setusakilanasrin@gmail.com>
2021-02-04 15:52:08 +0600

    data reverse

--- utils/fcm.py
+++ utils/fcm.py
@@ -55,8 +55,7 @@ def send_fcm_push_notification_appointment(tokens_list: list, status="CallStaff"
             'notification': {'title': 'Calling Waiter',
                              #  'image': "http://manager.i-host.com.bd/logo.png",
                              'body': f'Customer from table no {str(table_no)} is looking for you'},
-            'data': {'title': 'Calling Waiter',
-                             'body': f'Customer from table no {str(table_no)} is looking for you'}
+            'data': {'title': '7', 'body': str(datetime.datetime.now())}
         },
         'CallStaffForPayment': {
             'notification': {'title': 'Calling Waiter for payment',
